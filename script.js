776e3abcb57ec5b5e86142d4e25384deedb05968
EloK6 <ecassignol@gmail.com>
2018-11-05 23:24:13 +0100

    mouvement OK des éléments

--- script.js
+++ script.js
@@ -1,172 +1,283 @@
+var score = 0;
+var imageMermanNo = new Image();
+imageMermanNo.src = "./images/Merman_N0.png";
+var imageMermanYes = new Image();
+imageMermanYes.src = "./images/Merman_YES.png";
 
-window.onload = function() {
-    function drawCanvas() {
-        var canvas = document.getElementById('canvas');
-        ctx = canvas.getContext('2d');
-        ctx.clearRect(0, 0, 1300, 700);
-    }
-    drawCanvas();
-    
-    var score = 0;
+function drawCanvas() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    clearCanvas();
+}
+drawCanvas();
 
-    //Sky
-    function drawSky() {
-        ctx.beginPath();
-        ctx.moveTo(0, 100);
-        ctx.bezierCurveTo(canvas.width/2, canvas.height/3, 2*canvas.width/3, 50, canvas.width, 100);
-        ctx.lineTo(1300, 0);
-        ctx.lineTo(0,0);
-        ctx.closePath();
-        ctx.fillStyle ="#AFE4DB";
-        ctx.strokeStyle ="#AFE4DB";
-        ctx.fill();
-        ctx.stroke();
-    }
-    drawSky();
-
-    //cloud 
-    function drawCloud() {
-        var img = new Image();
-        img.onload = function() {
-            ctx.drawImage(img, 50, 20, 90, 50);
-            ctx.drawImage(img, 600, -50, 150, 100);
-            ctx.drawImage(img, 1200, -30, 150, 100);
-        };
-    img.src = "./images/Cloud.png";
-    }
-    drawCloud();
-
-    //SeaForeground
-    // Reste à animer les courbes de Bezier
-    function drawSeaForeground() {
-        ctx.beginPath();
-        ctx.moveTo(0, 100);
-        ctx.bezierCurveTo(canvas.width/2, canvas.height/3, 2*canvas.width/3, 50, canvas.width, 100);
-        ctx.lineTo(1300, 600);
-        ctx.lineTo(0, 700);
-        ctx.closePath();
-        ctx.fillStyle ="#68EAED";
-        ctx.strokeStyle ="#68EAED";
-        ctx.fill();
-        ctx.stroke();
-    }
-    drawSeaForeground();
-
-    //SeaBackground
-    // Reste à animer les courbes de Bezier
-    function drawSeaBackground() {
-        ctx.beginPath();
-        ctx.moveTo(0, 100);
-        ctx.bezierCurveTo(canvas.width/2, 50, 2*canvas.width/3, canvas.height/3, canvas.width, 100);
-        ctx.lineTo(1300, 600);
-        ctx.lineTo(0, 700);
-        ctx.closePath();
-        ctx.fillStyle ="#45E8F2";
-        ctx.strokeStyle ="#45E8F2";
-        ctx.fill();
-        ctx.stroke();
-    }
-    drawSeaBackground();
-
-    //Reef (a creer si le temps)
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
 
-    //Sand
-    function drawSand() {
-        ctx.beginPath();
-        ctx.moveTo(0, 600);
-        ctx.bezierCurveTo(canvas.width/3, canvas.height, 2*canvas.width/3, 550, canvas.width, 600);
-        ctx.lineTo(1300, 700);
-        ctx.lineTo(0, 700);
-        ctx.closePath();
-        ctx.fillStyle ="#E2E1C9";
-        ctx.strokeStyle ="#E2E1C9";
-        ctx.fill();
-        ctx.stroke();
-    }
-    drawSand();
-
-    function drawOctopus() {
-        var randomX = Math.floor(Math.random()*(1300)-50);
-        var randomY = Math.floor(Math.random()*(600-200)+150);
-        var img = new Image();
-        img.onload = function() {
-            ctx.drawImage(img, randomX, randomY, 60, 60);
-            ctx.drawImage(img, randomX+50, randomY+50, 60, 60);
-            ctx.drawImage(img, randomX-50, randomY-50, 60, 60);
-        };
-        img.src = "./images/Octopus.png";
-    }
-    drawOctopus();
-
-    var merman = {
-        x : 100,
-        y : 400,
-        moveUp : function() { this.y -= 5},
-        moveDown : function() { this.y += 5}
-    }
-
-    function draw(merman) {
-        var img = new Image();
-        img.onload = function() {
-            ctx.drawImage(img, merman.x, merman.y, 200, 100);
-        };
-    img.src = "./images/Merman_game.png";
-    }
-
-    document.onkeydown = function(e) {
-        switch (e.keyCode) {
-            case 38 : merman.moveUp(); break;
-            case 40 : merman.moveDown(); break;
-        }
-        updateCanvas();
-    }
-
-    function updateCanvas() {
-        ctx.clearRect(0, 0, 1300, 700);
-        draw(merman);
-        drawSky();
-        drawCloud();
-        drawSeaForeground();
-        drawSeaBackground();
-        drawSand();
-    }
-
-    updateCanvas();
-
-    function drawScore() {
-        var img = new Image();
-        img.onload = function() {
-            ctx.drawImage(img, 1250, 20, 40, 40);
-        };
-        img.src = "./images/Star_game.png";
-        ctx.font = "36px Pacifico";
-        ctx.textBaseline = "top";
-        ctx.textAlign = "left";
-        ctx.fillStyle="#F4A506";
-        ctx.fillText(score, 1220, 22);
-    }
+//-------------SetInterval---------------//
+var IntervalId = function() {
+    cloud.move();
+    octopus.move();
+    orshins.move();
+    merman.moveUp();
+    merman.moveDown();
+    star.move();
     drawScore();
+};
 
-    function gameOver() {
-        clearInterval(idIntervalle);
-        var img = new Image();
-        img.onload = function() {
-            ctx.drawImage(img, 1250, 20, 40, 40);
-        };
-        img.src = "./images/Star_game.png";
+
+setInterval(IntervalId, 30); 
+
+
+//-------------Background---------------//
+var numberOfLines = 2;
+var i = 0;
+var j = 0;
+
+var background = function() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    //sky
+    ctx.fillStyle = "#AFE4DB";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    //Sea
+    for(var k=0; k<numberOfLines; k++) {
+        var offset = (i+k*10)/30;
+        var y= 150;
+        var cpy1 = (Math.sin(offset)+1)*100;
+        var cpy2 = 150 - cpy1;
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.bezierCurveTo(canvas.width/3, cpy1, 2*canvas.width/3, cpy2, canvas.width, y);
+        ctx.lineTo(canvas.width, canvas.height);
+        ctx.lineTo(0, canvas.height);
+        ctx.closePath();
+        ctx.fillStyle = 'rgb(' + Math.floor(173-(k*104))+ ', 232, 242)';
+        ctx.strokeStyle = 'rgb(' + Math.floor(173-(k*104))+ ', 232, 242)';
+        ctx.fill();
+        ctx.stroke();
+
     }
-    gameOver();
+    i++;
 
-    // function Component(width, height, image, x, y) {
-    //     this.image = new Image();
-    //     this.image.src = image;
-    //     this.width = width;
-    //     this.height = height;
-    //     this.x = x;
-    //     this.y = y;
-    //     this.speedX = 0;
-    //     this.speedY = 0;
-    // }
+    // //Sand
+    ctx.beginPath();
+    ctx.moveTo(0, 600);
+    ctx.bezierCurveTo(canvas.width/3, canvas.height, 2*canvas.width/3, 550, canvas.width, 600);
+    ctx.lineTo(1300, 700);
+    ctx.lineTo(0, 700);
+    ctx.closePath();
+    ctx.fillStyle ="#E2E1C9";
+    ctx.strokeStyle ="#E2E1C9";
+    ctx.fill();
+    ctx.stroke();
+};
+setInterval(background, 30); 
+
+//-------------Cloud---------------//
+
+var imageCloud = new Image();
+imageCloud.onload = updateCanvas;
+
+var cloud = {
+    imageCloud : imageCloud,
+    x : 50,
+    speedX : -0.25,
+
+    move: function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    },
+
+    draw : function() {
+        ctx.drawImage(this.imageCloud, this.x, 20, 90, 50);
+        ctx.drawImage(this.imageCloud, this.x+550, -50, 150, 100);
+        ctx.drawImage(this.imageCloud, this.x+1220, -30, 150, 100);
+    }
+};
+imageCloud.src = "./images/Cloud.png";
+
+//-------------Octopus---------------//
+// var obstacles = [];
+
+// obstacles.push("octopus"); 
+    
+var imageOctopus = new Image();
+imageOctopus.onload = updateCanvas;
+
+var octopus = {
+    imageOctopus : imageOctopus,
+    x : canvas.width,
+    y : Math.floor(Math.random()*(600-250)+200),
+    speedX : -8,
+    speedY : 0.1,
+
+    move: function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.x <=-100 || this.y <= 0) { 
+            this.x = canvas.width; 
+            this.y = Math.floor(Math.random()*(600-200)+200);
+        }
+    },
+
+    draw : function() {
+        ctx.drawImage(this.imageOctopus, this.x, this.y, 70, 70);
+        ctx.drawImage(this.imageOctopus, this.x-50, this.y-50, 70, 70);
+        ctx.drawImage(this.imageOctopus, this.x-100, this.y-100, 70, 70);
+    }
 
 };
+imageOctopus.src = "./images/Octopus.png";
+
+//-------------Orshins---------------//
+    
+var imageOrshins = new Image();
+imageOrshins.onload = updateCanvas;
+
+var orshins = {
+    imageOrshins : imageOrshins,
+    x : 1400,
+    y : Math.floor(Math.random()*(600-250)+200),
+    speedX : -6,
+    speedY : 0.25,
+
+    move: function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.x <=-100 || this.y <= 0) { 
+            this.x = canvas.width; 
+            this.y = Math.floor(Math.random()*(600-200)+200);
+        }
+    },
+
+    draw : function() {
+        ctx.drawImage(this.imageOrshins, this.x, this.y, 40, 40);
+        ctx.drawImage(this.imageOrshins, this.x-30, this.y-50, 40, 40);
+    }
+
+};
+imageOrshins.src = "./images/Orshin.png";
+
+//-------------Stars---------------//
+
+var imageStars = new Image();
+imageStars.onload = updateCanvas;
+
+var star = {
+    imageStars : imageStars,
+    x : canvas.width,
+    y : Math.floor(Math.random()*(600-200)+200),
+    speedX : -5,
+    speedY : -0.05,
+
+    move: function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.x <=-120 || this.y <= 0) { 
+            this.x = canvas.width; 
+            this.y = Math.floor(Math.random()*(600-200)+200);
+        }
+    },
+
+    draw : function() {
+        ctx.drawImage(this.imageStars, this.x, this.y, 40, 40);
+        ctx.drawImage(this.imageStars, this.x+30, this.y+30, 40, 40);
+        ctx.drawImage(this.imageStars, this.x+60, this.y+60, 40, 40);
+        ctx.drawImage(this.imageStars, this.x+90, this.y+90, 40, 40);
+        ctx.drawImage(this.imageStars, this.x+120, this.y+120, 40, 40);
+    }
+};
+imageStars.src = "./images/Star_game.png";
+
+//-------------Merman---------------//
+
+var imageMerman = new Image();
+var imageMermanDown = new Image();
+var imageMermanUp = new Image();
+
+imageMerman.onload = updateCanvas;
+
+
+var merman = {
+    imageMerman : imageMerman,
+    x : 100,
+    y : 400,
+    speedX : 0,
+    speedY : 8,
+
+    moveUp: function() {
+        this.y -= this.speedY;
+    },
+     moveDown: function() {
+        this.y += this.speedY;
+    },
+
+    draw : function() {
+        ctx.drawImage(this.imageMerman, this.x, this.y, 220, 150);
+    }
+};
+
+document.onkeydown = function(e) {
+    if (e.keyCode == 38) {
+        merman.moveUp();
+    } if (e.keyCode == 40) {
+        merman.moveDown();
+    } if (e.keyCode == 40 || e.keyCode == 38) {
+        if(imageMerman.src != imageMermanDown.src) {
+            imageMerman.src = imageMermanDown.src;
+        } else {
+            imageMerman.src = imageMermanUp.src;
+        }
+    }
+}
+
+imageMerman.src = "./images/Merman_DOWN.png";
+imageMermanDown.src = "./images/Merman_DOWN.png";
+imageMermanUp.src = "./images/Merman_UP.png";
+
+//-------------Score---------------//
+
+function drawScore() {
+    var img = new Image();
+    img.onload = function() {
+        ctx.drawImage(img, 1250, 20, 40, 40);
+    };
+    img.src = "./images/Star_game.png";
+    ctx.font = "36px Pacifico";
+    ctx.textBaseline = "top";
+    ctx.textAlign = "left";
+    ctx.fillStyle="#F4A506";
+    ctx.fillText(score, 1220, 22);
+}
+
+
+//-------------GameOver---------------//
+
+function gameOver() {
+    clearInterval(idIntervalle);
+    var img = new Image();
+    img.onload = function() {
+        ctx.drawImage(img, 1250, 20, 40, 40);
+    };
+    img.src = "./images/Star_game.png";
+}
+
+
+//-------------Update---------------//
+
+function updateCanvas() {
+    // clearCanvas();
+    drawScore();
+    cloud.draw();
+    octopus.draw();
+    orshins.draw();
+    star.draw();
+    merman.draw();
+    requestAnimationFrame(updateCanvas);
+}
+
+
